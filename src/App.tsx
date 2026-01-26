a6c317e6cfd32bbf4f5640e3c8986cced8b8c66b
Bibek Chaudhary <bibek4929@gmail.com>
2026-01-26 22:29:47 +0545

    chore: initialize  react app with typescript, install redux, router, axios and tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold text-blue-400">
+        Tailwind + React + TypeScript 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
