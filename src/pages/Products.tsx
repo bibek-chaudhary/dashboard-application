bf97a00ac700d39a2aaee8561eb7d11f5cfa1f82
Bibek Chaudhary <bibek4929@gmail.com>
2026-01-26 23:44:52 +0545

    feat: add products table component

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,23 +1,24 @@
+import ProductsTable from "../components/products/ProductsTable";
+
 const Products = () => {
   return (
     <div className="p-6">
       <div className="mb-4">
         <h1 className="text-2xl font-semibold">Products</h1>
-        <p className="text-gray-500">Browse and manage products data</p>
+        <p className="text-gray-500">
+          Browse and manage products data
+        </p>
       </div>
 
-      {/* Search will go here */}
       <div className="mb-4">
+        {/* Search placeholder */}
         <div className="h-10 bg-gray-100 rounded" />
       </div>
 
-      {/* Table will go here */}
-      <div className="mb-4">
-        <div className="h-64 bg-gray-100 rounded" />
-      </div>
+      <ProductsTable products={[]} />
 
-      {/* Pagination will go here */}
-      <div className="flex justify-end">
+      <div className="flex justify-end mt-4">
+        {/* Pagination placeholder */}
         <div className="h-8 w-40 bg-gray-100 rounded" />
       </div>
     </div>
