660cad87a70ce4b5aeb50ca424601c74bc5f1c3e
Bibek Chaudhary <bibek4929@gmail.com>
2026-01-26 23:30:27 +0545

    feat: add products page skeleton

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,10 +1,25 @@
 const Products = () => {
   return (
-    <div>
-      <h1 className="text-2xl font-semibold mb-4">Products</h1>
-      <p className="text-gray-600">
-        Product data will be displayed here.
-      </p>
+    <div className="p-6">
+      <div className="mb-4">
+        <h1 className="text-2xl font-semibold">Products</h1>
+        <p className="text-gray-500">Browse and manage products data</p>
+      </div>
+
+      {/* Search will go here */}
+      <div className="mb-4">
+        <div className="h-10 bg-gray-100 rounded" />
+      </div>
+
+      {/* Table will go here */}
+      <div className="mb-4">
+        <div className="h-64 bg-gray-100 rounded" />
+      </div>
+
+      {/* Pagination will go here */}
+      <div className="flex justify-end">
+        <div className="h-8 w-40 bg-gray-100 rounded" />
+      </div>
     </div>
   );
 };
